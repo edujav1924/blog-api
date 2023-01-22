a5856d29fc7bac74f6883414bed9ace92c62fb4e
edujav <edujav21@gmail.com>
2023-01-22 16:39:06 -0300

    added authorization check middleware

--- src/v1/routes/userRoutes.js
+++ src/v1/routes/userRoutes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import userControllers from "../controllers/userControllers.js";
+import authRequired from "../../middlewares/auth.js";
 const router = Router();
 // version of user API v1
 // define routes for users
 
 router
-  .get("/", userControllers.getUserList)
-  .get("/:userId", userControllers.getUser)
-  .get("/:userId/posts", userControllers.getUserPostList)
-  .post("/:userId/posts", userControllers.createNewUserPost);
+  .get("/", authRequired, userControllers.getUserList)
+  .get("/:userId", authRequired, userControllers.getUser)
+  .get("/:userId/posts", authRequired, userControllers.getUserPostList)
+  .post("/:userId/posts", authRequired, userControllers.createNewUserPost);
 
 export default router;
