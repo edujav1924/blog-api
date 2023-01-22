2792118001bd78da6f4d47c4678f737b36ec893f
edujav <edujav21@gmail.com>
2023-01-22 12:29:15 -0300

    set fetch function default for express.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import v1Routes from "./v1/routes/userRoutes.js";
+import fetch from "node-fetch";
+global.fetch = fetch;
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
